1c14306b1490b8374042aa92d4a0317dcc64fc03
codeburner13 <kashiryaeva@gmail.com>
2022-04-25 20:54:51 +0300

    домашнее задание 3

---
+++ tp/src/views/HomeView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home">
+    <header>
+      <div class="title">My personal costs</div>
+    </header>
+    <main>
+      <PaymentsDisplay :items="paymentsList"/>
+      <AddPaymentForm @addNewPayment="addPaymentData" />
+    </main>
+  </div>
+</template>
+
+<script>
+import PaymentsDisplay from "@/components/PaymentsDisplay.vue";
+import AddPaymentForm from "@/components/AddPaymentForm.vue";
+export default {
+  name: "HomeView",
+  components: {
+    PaymentsDisplay,
+    AddPaymentForm
+},
+  data() {
+    return {
+      paymentsList: [],
+    };
+  },
+  methods: {
+    addPaymentData(data) {
+      this.paymentsList.push(data)
+    },
+    fetchData() {
+      return [
+        {
+          date: "28.03.2020",
+          category: "Food",
+          value: 169,
+        },
+        {
+          date: "24.03.2020",
+          category: "Transport",
+          value: 360,
+        },
+        {
+          date: "24.03.2020",
+          category: "Food",
+          value: 532,
+        },
+      ];
+    },
+  },
+  created() {
+    this.paymentsList = this.fetchData()
+  },
+  mounted() {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 20px;
+}
+</style>
